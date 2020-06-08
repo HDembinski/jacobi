b54fb67cbcf7ecbfe54a6efc75ced3713e9a4b63
Hans Dembinski <hans.dembinski@gmail.com>
2020-06-09 00:14:36 +0200

    u

--- src/jacobi/complex_step.py
+++ src/jacobi/complex_step.py
@@ -3,8 +3,7 @@ import numpy as np
 
 def jacobi(f, x):
     step = np.finfo(float).eps
-    c_dtype_str = np.dtype(step).str.replace("f", "c")
-    cx = np.asarray(x, dtype=c_dtype_str)
+    cx = np.asarray(x, dtype=complex)
     cx.imag = step
     r = f(cx).imag / step
     return r
