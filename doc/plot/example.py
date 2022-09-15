0314e69daf952a87d674a11bd8370c44b062d31c
Hans Dembinski <hans.dembinski@gmail.com>
2022-09-15 18:18:55 +0200

    update example plot

--- doc/plot/example.py
+++ doc/plot/example.py
@@ -4,17 +4,25 @@ from jacobi import jacobi
 
 
 # function of one variable with auxiliary argument; returns a vector
-def f(p, x):
-    y = p + x
-    return np.sin(y) / y
+def f(x):
+    return np.sin(x) / x
 
 
-x = np.linspace(-10, 10, 1000)
-fx = f(0, x)
-fdx, fdex = jacobi(f, 0, x)
+x = np.linspace(-10, 10, 200)
+fx = f(x)
+fdx, fdxe = jacobi(f, x, diagonal=True)
 
-plt.plot(x, fx, label="f(x) = sin(x) / x")
-plt.plot(x, fdx, ls="--", label="f'(x)")
+plt.plot(x, fx, label="$f(x) = sin(x) / x$")
+plt.plot(x, fdx, ls="--", label="$f'(x)$")
+scale = 14
+plt.fill_between(
+    x,
+    fdx - fdxe * 10**scale,
+    fdx + fdxe * 10**scale,
+    label=f"$f'(x)$ error estimate$\\times \\, 10^{{{scale}}}$",
+    facecolor="C1",
+    alpha=0.5,
+)
 plt.legend()
 
 plt.savefig("doc/_static/example.svg")
