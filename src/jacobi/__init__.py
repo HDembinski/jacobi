efb1d5b949eaa8ee503929ca35a1d7ee07018a16
Hans Dembinski <hans.dembinski@gmail.com>
2022-09-15 13:52:14 +0200

    wip

--- src/jacobi/__init__.py
+++ src/jacobi/__init__.py
@@ -1,4 +1,6 @@
 """Fast numerical derivatives for analytic functions with arbitrary round-off error."""
 
-from .core import jacobi, propagate  # noqa
+from .core import jacobi, propagate
 from ._version import version as __version__  # noqa
+
+__all__ = ["jacobi", "propagate"]
