76efdf94b6c011d1ccc78023f268bbcfd8f83ec5
Hans Dembinski <hans.dembinski@gmail.com>
2022-09-16 10:11:19 +0200

    restructuring files

--- src/jacobi/__init__.py
+++ src/jacobi/__init__.py
@@ -1,6 +1,7 @@
 """Fast numerical derivatives for analytic functions with arbitrary round-off error."""
 
-from .core import jacobi, propagate
+from ._propagate import propagate
+from ._jacobi import jacobi
 from ._version import version as __version__  # noqa
 
 __all__ = ["jacobi", "propagate"]
