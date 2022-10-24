perform correct error propagation if jacobian is diagonal

src/jacobi/_propagate.py
@@ -135,7 +135,7 @@ def _propagate_full(fn, y: np.ndarray, x: np.ndarray, xcov: np.ndarray, **kwargs
 
     _check_x_xcov_compatibility(x_a, xcov)
 
-    jac = np.asarray(jacobi(fn, x_a, **kwargs)[0])
+    jac = jacobi(fn, x_a, **kwargs)[0]
 
     y_len = len(y) if y.ndim == 1 else 1
 
@@ -143,6 +143,11 @@ def _propagate_full(fn, y: np.ndarray, x: np.ndarray, xcov: np.ndarray, **kwargs
         jac = jac.reshape((y_len, len(x_a)))
     assert np.ndim(jac) == 2
 
+    # Check if jacobian is diagonal, count NaN as zero.
+    # This is important to speed up the product below and
+    # to get the right answer for covariance matrices that
+    # contain NaN values.
+    jac = _try_reduce_jacobian(jac)
     ycov = _jac_cov_product(jac, xcov)
 
     if y.ndim == 0:
@@ -213,3 +218,22 @@ def _check_x_xcov_compatibility(x: np.ndarray, xcov: np.ndarray):
     if xcov.ndim > 0 and len(xcov) != (len(x) if x.ndim == 1 else 1):
         # this works for 1D and 2D xcov
         raise ValueError("x and cov have incompatible shapes")
+
+
+def _try_reduce_jacobian(jac: np.ndarray):
+    if jac.ndim != 2 or jac.shape[0] != jac.shape[1]:
+        return jac
+    # if jacobian contains only off-diagonal elements
+    # that are zero or NaN, we reduce it to diagonal form
+    m = np.isnan(jac)
+    jac[m] = 0
+    if np.count_nonzero(_nodiag_view(jac)) == 0:
+        return np.diag(jac)
+    return jac
+
+
+def _nodiag_view(a: np.ndarray):
+    # https://stackoverflow.com/a/43761941/ @Divakar
+    m = a.shape[0]
+    p, q = a.strides
+    return np.lib.stride_tricks.as_strided(a[:, 1:], (m - 1, m), (p + q, q))

test/test_propagate.py
@@ -224,11 +224,6 @@ def test_on_nan():
 
     y, ycov = propagate(fn, x, xcov, diagonal=True)
 
-    # Beware: this produces a matrix with all NaNs
-    #   y_ref, ycov_ref = propagate(fn, x, xcov)
-    # The derivative cannot figure out that the off-diagonal elements
-    # of the jacobian are zero.
-
     y_ref = [2, np.nan, 5]
     assert_allclose(y, y_ref)
 
@@ -239,3 +234,10 @@ def test_on_nan():
     # ycov_ref = jac @ np.array(xcov) @ jac.T
     ycov_ref = [[12, np.nan, 8], [np.nan, np.nan, np.nan], [8, np.nan, 80]]
     assert_allclose(ycov, ycov_ref)
+
+    # propagate now detects the special case where jac is effectively diagonal
+    # and does the equivalent of propagate(fn, x, xcov, diagonal=True), which
+    # is nevertheless faster
+    y2, ycov2 = propagate(fn, x, xcov)
+    assert_allclose(y2, y_ref)
+    assert_allclose(ycov2, ycov_ref)